e1ca6e5264f33e52e9d111460814d00c4c50a83a
BlaziusMaximus <1231862.student@gmail.com>
2020-11-29 18:52:01 -0500

    Updates

---
+++ precache-manifest.5784c417fcc6289e7e791dcccca38bd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f00aa7128eb4fc55f169d804aecc12bc",
+    "url": "/dm-assistant/index.html"
+  },
+  {
+    "revision": "21cc09da9b38b255983e",
+    "url": "/dm-assistant/static/css/main.145fff16.chunk.css"
+  },
+  {
+    "revision": "cee5919c2e1c7869fc5f",
+    "url": "/dm-assistant/static/js/2.a6a7a38f.chunk.js"
+  },
+  {
+    "revision": "4e1daf238ae1a730ff9f8ecfd6e3b57c",
+    "url": "/dm-assistant/static/js/2.a6a7a38f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21cc09da9b38b255983e",
+    "url": "/dm-assistant/static/js/main.456449ac.chunk.js"
+  },
+  {
+    "revision": "c4f5c896225a08e6b83f",
+    "url": "/dm-assistant/static/js/runtime-main.7aea2daf.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/dm-assistant/precache-manifest.5784c417fcc6289e7e791dcccca38bd3.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dm-assistant/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
